1cce17f186e5a8df5449e5a584c8f10e294c5b5f
MrReder <73144687+MrReder@users.noreply.github.com>
2021-02-16 00:54:21 +0200

    Add files via upload

--- homework 1.js
+++ homework 1.js
@@ -13,3 +13,22 @@ console.log(sum);
 
 const roundedSum = Math.floor(mouse) + Math.floor(pcDisplay) + Math.floor(wifiRouter);
 console.log(roundedSum);
+
+const roundedSumHundred = function (roundedSum) {
+    return Math.ceil(roundedSum / 100) * 100;
+};
+console.log(roundedSumHundred(roundedSum));
+
+console.log(Boolean.apply(roundedSum)); //I`m not sure if it`s a right answer
+
+const rest = 500 - sum;
+console.log(rest);
+
+const avgPrices = sum / 3;
+console.log(avgPrices.toFixed(2));
+
+const bill = sum + (sum * Math.random());
+console.log(bill.toFixed(2));
+
+const netProfit = (sum / 2) - Math.random() * sum;
+console.log(netProfit.toFixed(2)); 
